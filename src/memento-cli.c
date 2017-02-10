100ea4a51f747c820668821896ff52c0efb6083b
Andrea Giacomo Baldan <a.g.baldan@gmail.com>
2017-02-10 18:20:45 +0100

    Clean up code, bugfix and rename

---
+++ src/memento-cli.c
@@ -0,0 +1,206 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <fcntl.h>
+#include <time.h>
+#include <sys/time.h>
+
+#define CMD_BUFSIZE 1024
+#define VERSION "0.0.1"
+
+void banner(void);
+void help(void);
+void command_loop(int, char *, char *);
+char *read_command(void);
+
+int main(int argc, char **argv) {
+    int sock_fd, port;
+    struct sockaddr_in serveraddr;
+    struct hostent *server;
+    char *hostname;
+
+    /* check command line arguments */
+    if (argc != 3) {
+        fprintf(stderr,"usage: %s <hostname> <port>\n", argv[0]);
+        exit(0);
+    }
+    hostname = argv[1];
+    port = atoi(argv[2]);
+
+    /* socket: create the socket */
+    sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock_fd < 0)
+        perror("ERROR opening socket");
+
+    /* gethostbyname: get the server's DNS entry */
+    server = gethostbyname(hostname);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host as %s\n", hostname);
+        exit(0);
+    }
+
+    /* build the server's address */
+    bzero((char *) &serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr,
+          (char *)&serveraddr.sin_addr.s_addr, server->h_length);
+    serveraddr.sin_port = htons(port);
+
+    /* connect: create a connection with the server */
+    if (connect(sock_fd, (const struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0) {
+        perror("ERROR connecting");
+        exit(0);
+    }
+
+    /* fcntl(sock_fd, F_SETFL, O_NONBLOCK); */
+
+    char c_port[4];
+    sprintf(c_port, "%d", port);
+    banner();
+    command_loop(sock_fd, hostname, c_port);
+    return EXIT_SUCCESS;
+}
+
+// loop for accepting, parsing and sending commands to the memento server
+// instance, reading response
+void command_loop(int fd, char *hostname, char *port) {
+    char *line, buf[CMD_BUFSIZE];
+    int status = 1;
+    // while condition: status > 0
+    while (status) {
+        printf("%s:%s> ", hostname, port);
+        line = read_command();
+        char supp_command[strlen(line)];
+        bzero(supp_command, strlen(line));
+        strcpy(supp_command, line);
+        char *comm = strtok(supp_command, " ");
+        if (strlen(line) > 1) {
+            char token[4];
+            strncpy(token, line, 4);
+            if (strncasecmp(token, "SUB ", 4) == 0 || strncasecmp(token, "TAIL", 4) == 0) {
+                // write to server
+                status = write(fd, line, strlen(line));
+                if (status < 0)
+                    perror("ERROR writing to socket");
+                while (1) {
+                    /* print the server's reply */
+                    bzero(buf, CMD_BUFSIZE);
+                    status = read(fd, buf, CMD_BUFSIZE);
+                    if (strncasecmp(buf, "NOT FOUND", 9) == 0) break;
+                    printf("%s", buf);
+                }
+            } else if (strncasecmp(comm, "KEYS", 4) == 0 ||
+                       strncasecmp(comm, "VALUES", 6) == 0 ||
+                       strncasecmp(comm, "PREFSCAN", 8) == 0 ||
+                       strncasecmp(comm, "FUZZYSCAN", 9) == 0) {
+                fcntl(fd, F_SETFL, O_NONBLOCK);
+                status = write(fd, line, strlen(line));
+                usleep(100000);
+                if (status < 0)
+                    perror("ERROR writing to socket");
+                while(read(fd, buf, CMD_BUFSIZE) > 0) {
+                    printf("%s", buf);
+                    bzero(buf, CMD_BUFSIZE);
+                }
+            } else if (token[0] == '?' ||
+                       token[0] == 'h' ||
+                       token[0] == 'H' ||
+                       strncasecmp(token, "HELP", 4) == 0) {
+                help();
+                bzero(buf, CMD_BUFSIZE);
+            /* } else if (strncasecmp(comm, "BENCHMARK", 9) == 0) { */
+            /*     /\* clock_t start_t, end_t; *\/ */
+            /*     /\* start_t = clock(); *\/ */
+            /*     struct timeval start, stop; */
+            /*     double secs = 0; */
+
+            /*     gettimeofday(&start, NULL); */
+
+            /*     for (int i = 0; i < 30000; i++) { */
+            /*         /\* char payload[30]; *\/ */
+            /*         /\* sprintf(payload, "set key%d value%d\r\n", i, i); *\/ */
+            /*         /\* printf("%s\n", payload); *\/ */
+            /*         char *payload = "set key value\r\n"; */
+            /*         status = write(fd, payload, 15); */
+            /*         /\* bzero(buf, CMD_BUFSIZE); *\/ */
+            /*         /\* status = read(fd, buf, CMD_BUFSIZE); *\/ */
+            /*     } */
+            /*     /\* end_t = clock(); *\/ */
+            /*     /\* double delta_t = (double) (end_t - start_t) / CLOCKS_PER_SEC; *\/ */
+            /*     gettimeofday(&stop, NULL); */
+            /*     secs = (double)(stop.tv_usec - start.tv_usec) / 1000000 + (double)(stop.tv_sec - start.tv_sec); */
+            /*     printf("Nr. of set ops: %d\n", 30000); */
+            /*     printf("CPU time: %lf\n", secs); */
+            } else {
+                // write to server
+                status = write(fd, line, strlen(line));
+                if (status < 0)
+                    perror("ERROR writing to socket");
+                /* print the server's reply */
+                bzero(buf, CMD_BUFSIZE);
+                status = read(fd, buf, CMD_BUFSIZE);
+                if (status < 0)
+                    perror("ERROR reading from socket");
+                printf("%s", buf);
+            }
+            free(line);
+        }
+    }
+}
+// read a single string containing command
+char *read_command(void) {
+    char *line = NULL;
+    size_t bufsize = 0;
+    getline(&line, &bufsize, stdin);
+    return line;
+}
+// print initial banner
+void banner(void) {
+    printf("\n");
+    printf("Memento CLI v %s\n", VERSION);
+    printf("Type h or ? or help to print command list\n\n");
+}
+// print help
+void help(void) {
+    printf("\n");
+    printf("SET key value               Sets <key> to <value>\n");
+    printf("GET key                     Get the value identified by <key>\n");
+    printf("DEL key key2 .. keyN        Delete values identified by <key>..<keyN>\n");
+    printf("INC key qty                 Increment by <qty> the value idenfied by <key>, if\n");
+    printf("                            no <qty> is specified increment by 1\n");
+    printf("DEC key qty                 Decrement by <qty> the value idenfied by <key>, if\n");
+    printf("                            no <qty> is specified decrement by 1\n");
+    printf("INCF                        key qty Increment by float <qty> the value identified\n");
+    printf("                            by <key>, if no <qty> is specified increment by 1.0\n");
+    printf("DECF                        key qty Decrement by <qty> the value identified by <key>,\n");
+    printf("                            if no <qty> is specified decrement by 1.0\n");
+    printf("SUB key key2 .. keyN        Subscribe to <key>..<keyN>, receiving messages published\n");
+    printf("UNSUB key key2 .. keyN      Unsubscribe from <key>..<keyN>\n");
+    printf("PUB key value               Publish message <value> to <key> (analog to set\n");
+    printf("                            but broadcasting to all subscribed members of key)\n");
+    printf("GETP key                    Get all information of a key-value pair represented by\n");
+    printf("                            <key>, like key, value, creation time and expire time\n");
+    printf("APPEND key value            Append <value> to <key>\n");
+    printf("PREPEND key value           Prepend <value> to <key>\n");
+    printf("EXPIRE key ms               Set an expire time in milliseconds after that the <key>\n");
+    printf("                            will be deleted, upon taking -1 as <ms> value the\n");
+    printf("                            expire time will be removed\n");
+    printf("KEYS                        List all keys stored into the keyspace\n");
+    printf("VALUES                      List all values stored into the keyspace\n");
+    printf("COUNT                       Return the number of key-value pair stored\n");
+    printf("TAIL key offset             Like SUB, but with an <offset> representing how\n");
+    printf("                            many messages discard starting from 0 (the very\n");
+    printf("                            first chronologically)\n");
+    printf("PREFSCAN key_prefix         Scan the keyspace finding all values associated to\n");
+    printf("                            keys matching <key_prefix> as prefix\n");
+    printf("FUZZYSCAN pattern           Scan the keyspace finding all values associated to\n");
+    printf("                            keys matching <pattern> in a fuzzy search way\n");
+    printf("FLUSH                       Delete all maps stored inside partitions\n");
+    printf("QUIT/EXIT                   Close connection\n");
+    printf("\n");
+}
---
+++ src/memento.c
@@ -0,0 +1,209 @@
+/*
+ * Copyright (c) 2016-2017 Andrea Giacomo Baldan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <getopt.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <time.h>
+#include "util.h"
+#include "cluster.h"
+#include "commands.h"
+#include "networking.h"
+
+
+static void *form_cluster_thread(void *p) {
+
+    int len;
+    sleep(2);
+
+    while (instance.lock == 1) {
+
+        list_node *cursor = instance.cluster->head;
+
+        while(cursor) {
+            /* count for UNREACHABLE nodes */
+            len = cluster_unreachable_count();
+            cluster_node *n = (cluster_node *) cursor->data;
+            if (cluster_reachable(n) == 0) {
+                LOG(DEBUG, "Trying to connect to cluster node %s:%d\n",
+                        n->addr, n->port);
+                char p[5];
+                sprintf(p, "%d", n->port);
+                if (cluster_join(n->addr, p) == 1) len--;
+            }
+            cursor = cursor->next;
+        }
+
+        if (len <= 0) instance.lock = 0; // all nodes are connected, release lock
+
+        LOG(DEBUG, "Instance lock: %u\n", instance.lock);
+        sleep(3);
+    }
+
+    LOG(DEBUG, "Cluster node succesfully joined to the cluster\n");
+    cluster_balance();
+    LOG(DEBUG, "All cluster nodes are balanced\n");
+    // debug check
+    // FIXME: remove
+    list_node *cursor = instance.cluster->head;
+    while(cursor) {
+        cluster_node *n = (cluster_node *) cursor->data;
+        LOG(DEBUG, "Node: %s:%d - Min: %u Max: %u Name: %s Fd: %d\n",
+                n->addr, n->port, n->range_min, n->range_max, n->name, n->fd);
+        cursor = cursor->next;
+    }
+    LOG(INFO, "Cluster succesfully formed\n");
+    return NULL;
+}
+
+
+int main(int argc, char **argv) {
+
+    /* Initialize random seed */
+    srand((unsigned int) time(NULL));
+    const char *home = get_homedir();
+    char *address = "127.0.0.1";
+    char *port = "8082";
+    char *confpath = "/.memento";    // default path for configuration ~/.memento
+    char *filename = malloc(strlen(home) + strlen(confpath));
+    sprintf(filename, "%s%s", home, confpath);
+    char *id = "A";
+    int opt, cluster_mode = 0;
+    static pthread_t thread;
+
+    while((opt = getopt(argc, argv, "a:i:p:cf:")) != -1) {
+        switch(opt) {
+            case 'a':
+                address = optarg;
+                break;
+            case 'p':
+                port = optarg;
+                break;
+            case 'i':
+                id = optarg;
+                break;
+            case 'c':
+                cluster_mode = 1;
+                break;
+            case 'f':
+                filename = optarg;
+                break;
+            default:
+                cluster_mode = 0;
+                break;
+        }
+    }
+
+    char bus_port[20];
+    int bport = GETINT(port) + 100;
+    sprintf(bus_port, "%d", bport);
+
+    /* initialize two sockets:
+     * - one for incoming client connections
+     * - a second for intercommunication between nodes
+     */
+    int sockets[2] = {
+        listento(address, port),
+        listento(address, bus_port)
+    };
+
+
+    /* handler function for incoming data */
+    fd_handler handler_ptr = &command_handler;
+
+    /* If cluster mode is enabled Initialize cluster map */
+    if (cluster_mode == 1) {
+
+        cluster_init(1, id, address, bus_port);
+
+        /* read cluster configuration */
+        FILE *file = fopen(filename, "r");
+        char line[256];
+        int linenr = 0;
+
+        if (file == NULL) {
+            perror("Couldn't open the configuration file\n");
+            free(filename);
+            exit(EXIT_FAILURE);
+        }
+
+        while (fgets(line, 256, (FILE *) file) != NULL) {
+
+            char *ip = malloc(15), *pt = malloc(5), *name = malloc(256);
+            int self_flag = 0;
+            linenr++;
+
+            /* skip comments line */
+            if (line[0] == '#') continue;
+
+            if (sscanf(line, "%s %s %s %d", ip, pt, name, &self_flag) != 4) {
+                fprintf(stderr, "Syntax error, line %d\n", linenr);
+                continue;
+            }
+
+            LOG(DEBUG, "[CFG] Line %d: IP %s PORT %s NAME %s SELF %d\n",
+                    linenr, ip, pt, name, self_flag);
+
+            /* create a new node and add it to the list */
+            cluster_node *new_node =
+                (cluster_node *) shb_malloc(sizeof(cluster_node));
+            new_node->addr = ip;
+            new_node->port = GETINT(pt);
+            new_node->state = UNREACHABLE;
+
+            if (self_flag == 1) {
+                /* check if the node is already present */
+                if (cluster_contained(new_node) == 1) {
+                    free(new_node);
+                    continue;
+                } else {
+                    new_node->self = 1;
+                    new_node->state = REACHABLE;
+                }
+            }
+            else new_node->self = 0;
+            new_node->name = name;
+
+            /* add the node to the cluster list */
+            cluster_add_node(new_node);
+
+        }
+
+        fclose(file);
+
+        /* start forming the thread */
+        if (pthread_create(&thread, NULL, &form_cluster_thread, NULL) != 0)
+            perror("ERROR pthread");
+
+    } else {
+
+        /* single node mode */
+        cluster_init(0, id, address, bus_port);
+    }
+
+    /* start the main listen loop */
+    event_loop(sockets, 2, handler_ptr);
+
+    return 0;
+}
